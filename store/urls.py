7a17eb7b9d12b488c07f6f4bc2b01fb8962cb1cb
NikaKhiz <khizambarelinika123@gmail.com>
2024-10-10 06:18:22 +0400

    fetch categories with parents feature

--- store/urls.py
+++ store/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
-from .views import *
 from djangostore import settings
 from django.conf.urls.static import static
+from .views import *
+
 
 urlpatterns = [
     # for this moment we visit products page by default
     path('',product_index, name='product-index'),
-    path('product/<str:id>',product_show, name='product-show')
+    path('product/<str:id>',product_show, name='product-show'),
+    path('categories',categories, name='categories'),
 ]
 
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import HttpResponse
-
+from django.http import HttpResponse, JsonResponse
+from .models import Category
 
 # Display provided products
 def product_index(request):
@@ -10,9 +10,22 @@ def product_index(request):
 def product_show(request,id):
     return HttpResponse(f'details for product - {id}', )
 
-
+# Fetch all categories with its parents
 def categories(request):
-    return HttpResponse('categories with parents')
+    
+    categories = Category.objects.select_related('parent_category').all()
+    context = {
+        'categories': [
+            {
+                'id': category.id,
+                'name': category.name,
+                'parent': category.parent_category.name if category.parent_category else None
+            } for category in categories
+        ]
+    }
+
+    return JsonResponse(context)
+
 
 def products(request):
     return HttpResponse('products with categories')
