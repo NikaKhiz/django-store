f99f47c1fee5fcc07fc1b735e64bf1978d78eeef
NikaKhiz <khizambarelinika123@gmail.com>
2024-10-14 04:04:43 +0400

    statis files saving conf move from store app urls into project urls file

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from djangostore import settings
-from django.conf.urls.static import static
 from .views import *
 
 
@@ -9,7 +7,3 @@ urlpatterns = [
     path('product/<str:id>',product_show, name='product-show'),
     path('categories',categories, name='categories'),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
