b5222a9b9e076286022c3db14de808a2ec8f6408
NikaKhiz <khizambarelinika123@gmail.com>
2024-10-15 04:20:58 +0400

    modifie urls

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('category',categories, name='categories'),
-    path('category/<str:id>/products',categories, name='category'),
+    path('category/', categories, name='category'),
+    path('category/<str:id>/products/',category_products, name='products'),
     path('product',product_show, name='product'),
 ]
