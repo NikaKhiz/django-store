295d5fa2aaf936f7dfef0b558d0855bfb309c19e
NikaKhiz <khizambarelinika123@gmail.com>
2024-10-15 19:11:34 +0400

    modifie media files configuration

--- djangostore/urls.py
+++ djangostore/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
 from debug_toolbar.toolbar import debug_toolbar_urls
-from django.conf import settings
 
 
 urlpatterns = [
@@ -10,7 +8,3 @@ urlpatterns = [
     path('', include('order.urls')),
     path('', admin.site.urls)
 ] + debug_toolbar_urls()
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -7,3 +9,7 @@ urlpatterns = [
     path('category/<str:id>/products/', category_products, name='products'),
     path('product/<slug:slug>/', product_show, name='product'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
