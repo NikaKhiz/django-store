0745b55d10b81268a4e07e6dbd5460ea40014b63
NikaKhiz <khizambarelinika123@gmail.com>
2024-10-17 16:27:20 +0400

    customuseradmin

--- userapp/admin.py
+++ userapp/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-admin.site.register(CustomUser, UserAdmin)
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'is_active', 'date_joined'  )
+    search_fields = ('username',)
+    ordering = ('username',)
+    list_editable = ('email', 'first_name', 'is_active')
+    
