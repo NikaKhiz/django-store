a939838ea50c9f77d5bd6a2e18735beb76a9a9aa
NikaKhiz <khizambarelinika123@gmail.com>
2024-10-17 06:46:55 +0400

    register models using modeladmin

--- store/admin.py
+++ store/admin.py
@@ -2,4 +2,34 @@ from django.contrib import admin
 from .models import Category, Product
 
 
-admin.site.register([Category, Product])
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent', 'created_at', 'updated_at')
+    search_fields = ('name',)
+    list_filter = ('parent',)
+    ordering = ('name',)
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'parent')
+        }),
+    )
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'is_published', 'created_at', 'updated_at')
+    search_fields = ('name', 'slug',)
+    list_filter = ('is_published', 'category')
+    filter_horizontal = ('category',)
+    prepopulated_fields = {'slug': ('name',)}
+    ordering = ('-created_at',)  
+
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'slug', ('price', 'quantity',), 'is_published', 'description' )
+        }),
+        ('Advanced options', {
+            'classes': ('collapse', ),
+            'fields': ('category', 'image')
+        }),
+    )
