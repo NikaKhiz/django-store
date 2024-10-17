12d2ca98cde01c880d4afec14ea896198f9099e4
NikaKhiz <khizambarelinika123@gmail.com>
2024-10-17 13:01:31 +0400

    modifie products and category modeladmin

--- store/admin.py
+++ store/admin.py
@@ -17,19 +17,24 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'quantity', 'is_published', 'created_at', 'updated_at')
+    list_display = ('name', 'price', 'quantity', 'total_prices', 'is_published', 'created_at', 'updated_at')
     search_fields = ('name', 'slug',)
     list_filter = ('is_published', 'category')
-    filter_horizontal = ('category',)
+    autocomplete_fields = ('category', )
     prepopulated_fields = {'slug': ('name',)}
-    ordering = ('-created_at',)  
+    ordering = ('-created_at',)
+    date_hierarchy = 'updated_at'
 
     fieldsets = (
         (None, {
-            'fields': ('name', 'slug', ('price', 'quantity',), 'is_published', 'description' )
+            'fields': ('name', 'slug', 'category', ('price', 'quantity'), 'is_published')
         }),
         ('Advanced options', {
             'classes': ('collapse', ),
-            'fields': ('category', 'image')
+            'fields': ('image', )
         }),
     )
+
+    @admin.display
+    def total_prices(self, obj):
+        return obj.price * obj.quantity
