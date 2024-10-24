c8aeb20b5bb476fe0b0494c4028ad434f067ab97
NikaKhiz <khizambarelinika123@gmail.com>
2024-10-24 08:06:21 +0400

    get products from db get products from db according to chosen categories add breadcrumbs in navigation

--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
 from .models import Category, Product
 from django.shortcuts import render, get_object_or_404
-from django.db.models import Count, F, Sum, Max, Min, Avg
+from django.db.models import Count, F, Sum, Max, Min, Avg, Prefetch
 from django.core.paginator import Paginator
 
 products = []
@@ -20,25 +20,50 @@ for _ in range(6):
         }
     )
 
-categories = []
-for _ in range(6):
-    categories.append(
-        {
-            'title': 'Fruits',
-            'slug': 'fruits',
-            'description':'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt',
-            'image':'img/fruite-item-1.jpg'
-        }
-    )
-
 def index(request):
-    
-    context = {'categories': categories}
+    root_categories = Category.objects.filter(parent=None)
+
+    categories = Category.objects.prefetch_related(Prefetch(
+        'products', queryset=Product.objects.prefetch_related('tag')
+    ))
+
+    products = []
+    for category in categories:
+        products.extend(category.products.all())
+
+    context = {'root_categories': root_categories,'categories': categories, 'products': products}
+
     return render(request, 'index.html', context)
 
+def category_products(request, slug=None):
+    root_categories = Category.objects.filter(parent=None)
+    
+    if slug:
+        categories = Category.objects.filter(slug=slug).prefetch_related(Prefetch(
+            'products', queryset=Product.objects.prefetch_related('tag')
+        ))
+    else:
+        categories = Category.objects.prefetch_related(Prefetch(
+            'products', queryset=Product.objects.prefetch_related('tag')
+        ))
+    
+    products = []
+    for category in categories:
+        products.extend(category.products.all())
 
-def category_products(request, slug='fruits'):
-    context = {'products': products}
+
+    breadcrumb = [('Shop', 'store:products')]
+    if slug:
+        breadcrumb.append((categories[0].name,'store:products'))
+
+    context = {
+        'root_categories': root_categories,
+        'categories': categories, 
+        'products': products,
+        'slug': slug,
+        'breadcrumb': breadcrumb
+        }
+
     return render(request, 'category_products.html', context)
 
 
