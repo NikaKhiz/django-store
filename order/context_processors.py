7f808b00f5831192df34732c63746d0a1f24ccd3
NikaKhiz <khizambarelinika123@gmail.com>
2024-10-29 06:22:22 +0400

    total products default value

--- order/context_processors.py
+++ order/context_processors.py
@@ -2,10 +2,11 @@ from django.db.models import Sum
 from .models import Usercart
 
 def cart_items_context(request):
-    cart = None
+    total_products = 0
     if request.user.is_authenticated:
         cart = Usercart.objects.filter(user=request.user).first()
-    total_products = cart.cart_items.aggregate(total=Sum('quantity'))['total'] if cart else 0
+        if cart:
+            total_products = cart.cart_items.aggregate(total=Sum('quantity'))['total'] or 0
 
     return {
         'total_products': total_products,
