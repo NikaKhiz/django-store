76d9d77f9d313950bf3b17dcceee5bdc9257c193
NikaKhiz <khizambarelinika123@gmail.com>
2024-10-30 03:51:21 +0400

    user authorization forms and views

--- userapp/views.py
+++ userapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+
+class LoginUserView(LoginView):
+    template_name='login.html'
+
+
+class RegisterUserView(View):
+    
+    def get(self, request):
+        
+        return render(request, 'register.html')
+
+    def post(self, request):
+        ...
+
+
+class LogoutUserView(LoginRequiredMixin, LogoutView):
+    next_page='store:index'
