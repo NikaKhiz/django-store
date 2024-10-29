ecf102e1b9feafb5f26e6d77bf82bb38c5ad3f0b
NikaKhiz <khizambarelinika123@gmail.com>
2024-10-30 03:50:53 +0400

    templates for login and register custom js file for handling user menu toggling

---
+++ static/js/custom.js
@@ -0,0 +1,15 @@
+button = document.getElementById("btn-user");
+userMenu = document.getElementById("user-menu");
+button.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (userMenu.style.display === "none" || userMenu.style.display === "") {
+    userMenu.style.display = "block";
+  } else {
+    userMenu.style.display = "none";
+  }
+});
+window.addEventListener("click", (event) => {
+  if (event.target !== button && !userMenu.contains(event.target)) {
+    userMenu.style.display = "none";
+  }
+});
