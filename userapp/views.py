f81185abfbca632d306718b8a44859e1fed89491
NikaKhiz <khizambarelinika123@gmail.com>
2024-10-31 03:28:34 +0400

    user login template and view for user login feature

--- userapp/views.py
+++ userapp/views.py
@@ -1,23 +1,7 @@
 from django.shortcuts import render
-from django.contrib.auth.views import LoginView, LogoutView
-from django.views import View
-from django.contrib.auth.mixins import LoginRequiredMixin
-
+from django.contrib.auth.views import LoginView
 
 
 class LoginUserView(LoginView):
     template_name='login.html'
-
-
-class RegisterUserView(View):
-    
-    def get(self, request):
-        
-        return render(request, 'register.html')
-
-    def post(self, request):
-        ...
-
-
-class LogoutUserView(LoginRequiredMixin, LogoutView):
-    next_page='store:index'
+    next_page = 'store:index'
