d3f52c286c2e687f390b7d0d7d42fe81ff85bf56
NikaKhiz <khizambarelinika123@gmail.com>
2024-10-31 03:58:11 +0400

    give staff status new users on register

--- userapp/views.py
+++ userapp/views.py
@@ -10,6 +10,13 @@ class RegisterUserView(CreateView):
     template_name = 'register.html'
     success_url = reverse_lazy('store:index')
 
+    def form_valid(self, form):
+        """If the form is valid, save the associated model."""
+        self.object = form.save(commit=False)
+        self.object.is_staff = True
+        form.save()
+        return super().form_valid(form)
+
    
 class LoginUserView(LoginView):
     template_name='login.html'
