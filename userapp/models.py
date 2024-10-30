8ab3356b2210044803c0ae6d14417b49fe3631dc
NikaKhiz <khizambarelinika123@gmail.com>
2024-10-31 00:41:03 +0400

    custom user model and admin

--- userapp/models.py
+++ userapp/models.py
@@ -2,5 +2,48 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+
+
 class CustomUser(AbstractUser):
-    pass
+    def create_user(self, username, email, password=None):
+        """
+        Creates and saves a User with the given username, email and password.
+        """
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not username:
+            raise ValueError("Users must username")
+        if not password:
+            raise ValueError("Users must password")
+            
+
+        user = self.model(
+            username=self.normalize_username(username),
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        """
+        Creates and saves a User with the given username, email and password.
+        """
+        user = self.create_user(
+            username=self.normalize_username(username),
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    username=models.CharField(max_length=40,unique=True)
+    last_active_datetime = models.DateTimeField(auto_now=True)
+    email=models.EmailField(max_length=255,unique=True)
+
+    def __str__(self):
+        return self.username
+
+        
+        
