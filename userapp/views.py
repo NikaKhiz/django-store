063c9acada531467cebc426129974e19b77f554d
NikaKhiz <khizambarelinika123@gmail.com>
2024-10-31 03:29:33 +0400

    view for user logout feature

--- userapp/views.py
+++ userapp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class LoginUserView(LoginView):
     template_name='login.html'
     next_page = 'store:index'
+
+
+class LogoutUserView(LoginRequiredMixin, LogoutView):
+    next_page='store:index'
