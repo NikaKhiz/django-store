87ac8130831d4507023f361b3e7e27707a5e2a7b
NikaKhiz <khizambarelinika123@gmail.com>
2024-11-03 10:43:22 +0400

    define per view cache on products listing

--- store/urls.py
+++ store/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 from .views import *
 from django.views.generic import TemplateView
+from django.views.decorators.cache import cache_page
 
 app_name = 'store'
 
 urlpatterns = [
     # main page
-    path('', Indexview.as_view(), name='index'),
+    path('', cache_page(60 * 1)(Indexview.as_view()), name='index'),
     # products listing by categories
-    path('category/', CategoryProductsView.as_view(), name='products'),
-    path('category/<slug:slug>/', CategoryProductsView.as_view(), name='products'),
+    path('category/', cache_page(60 * 1)(CategoryProductsView.as_view()), name='products'),
+    path('category/<slug:slug>/', cache_page(60 * 1)(CategoryProductsView.as_view()), name='products'),
     # product detailed page
     path('product/', ProductView.as_view(), name='product'),
     path('product/<slug:slug>/', ProductView.as_view(), name='product'),
